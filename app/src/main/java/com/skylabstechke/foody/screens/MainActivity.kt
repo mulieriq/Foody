2b43c24e0e974ee06f30ea09bf0c2b1ba35cda17
EricRootLee <erickiokom@gmail.com>
2021-01-19 20:49:55 +0300

    LInked navhost to main activity

--- app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
@@ -2,11 +2,18 @@ package com.skylabstechke.foody.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.skylabstechke.foody.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.navHostMain))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return  findNavController(R.id.navHostMain).navigateUp() || super.onSupportNavigateUp()
     }
 }
