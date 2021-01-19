5bc4b8f6ba774afe4d60de6bb449f41fa85dfca8
EricRootLee <erickiokom@gmail.com>
2021-01-19 19:58:55 +0300

    REFACTORED MAIN

--- app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.skylabstechke.foody
+package com.skylabstechke.foody.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skylabstechke.foody.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
