d8b8e1a9d4d05d0f2f8862c118a5409f34802f9f
EricRootLee <erickiokom@gmail.com>
2021-01-20 13:36:42 +0300

    Refactored frgments

--- app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
@@ -3,10 +3,7 @@ package com.skylabstechke.foody.screens
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.skylabstechke.foody.R
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/skylabstechke/foody/screens/fragments/RecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/screens/fragments/RecipesFragment.kt
@@ -11,7 +11,7 @@ class RecipesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_recipes, container, false)
     }
 
