2a3ce6dce94a1615f302d3eaa153ec5e252f21f9
EricRootLee <erickiokom@gmail.com>
2021-01-20 13:44:31 +0300

    edited menu

--- app/src/main/java/com/skylabstechke/foody/ui/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/MainActivity.kt
@@ -10,11 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         setupActionBarWithNavController(findNavController(R.id.navHostMain))
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
