7f631cf09dbbffdedb6a1ad2bb991b88aa14aaae
EricRootLee <erickiokom@gmail.com>
2021-01-20 11:43:27 +0300

    Linked bottom nav with nav cotroller

--- app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/screens/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.skylabstechke.foody.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.skylabstechke.foody.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupActionBarWithNavController(findNavController(R.id.navHostMain))
+
+        val appBarConfig = AppBarConfiguration(setOf(R.id.recipesFragment, R.id.favoriteRecipesFragment, R.id.foodJokeFragment))
+
+        setupActionBarWithNavController(findNavController(R.id.navHostMain),appBarConfig)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).apply {
+            setupWithNavController(findNavController(R.id.navHostMain))
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return  findNavController(R.id.navHostMain).navigateUp() || super.onSupportNavigateUp()
+        return findNavController(R.id.navHostMain).navigateUp() || super.onSupportNavigateUp()
     }
 }
