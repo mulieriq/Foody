16284688b48f3f3958f2712c0c58fcf0ae2160ff
EricRootLee <erickiokom@gmail.com>
2021-04-10 02:38:49 +0300

    results clean

--- app/src/main/java/FoodRecipe.kt
+++ app/src/main/java/FoodRecipe.kt
@@ -2,12 +2,8 @@
 import com.google.gson.annotations.SerializedName
 
 data class FoodRecipe(
-    @SerializedName("number")
-    val number: Int,
-    @SerializedName("offset")
-    val offset: Int,
+
     @SerializedName("results")
     val results: List<Result>,
-    @SerializedName("totalResults")
-    val totalResults: Int
+
 )
