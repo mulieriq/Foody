db9add3367be0c8782f05d9ae79e05c9af00cf92
EricRootLee <erickiokom@gmail.com>
2021-04-10 02:56:42 +0300

    done

--- app/src/main/java/com/skylabstechke/foody/ui/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import com.skylabstechke.foody.R
 
 //Root ui
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/skylabstechke/foody/utilis/Constants.kt
+++ app/src/main/java/com/skylabstechke/foody/utilis/Constants.kt
@@ -1,7 +1,8 @@
 package com.skylabstechke.foody.utilis
 
 class Constants {
-    companion object{
-
+    companion object {
+        const val API_KEY = "3ebfff5f4fd849bea702b5b5df175767";
+        const val BASE_URL = "https://api.spoonacular.com"
     }
 }
