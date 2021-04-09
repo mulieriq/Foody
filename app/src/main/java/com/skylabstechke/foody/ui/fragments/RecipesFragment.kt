5af063cac3495c3a2d1b713a04665b7c6257522f
EricRootLee <erickiokom@gmail.com>
2021-04-10 01:40:10 +0300

    added extension synthetics

--- app/src/main/java/com/skylabstechke/foody/ui/fragments/RecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/fragments/RecipesFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.skylabstechke.foody.R
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
+
 
 class RecipesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view =  inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.recyclerview.showShimmer()
+        return  view
     }
 
 }
