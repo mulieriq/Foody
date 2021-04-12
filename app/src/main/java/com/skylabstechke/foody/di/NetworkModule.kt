5a30be6702b43ac2dc185df8a089048d1cd9c533
EricRootLee <erickiokom@gmail.com>
2021-04-12 13:50:46 +0300

    Network object module

--- app/src/main/java/com/skylabstechke/foody/di/NetworkModule.kt
+++ app/src/main/java/com/skylabstechke/foody/di/NetworkModule.kt
@@ -1,4 +1,16 @@
 package com.skylabstechke.foody.di
 
+import com.skylabstechke.foody.data.FoodRecipiesApi
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(ApplicationComponent::class)
 object NetworkModule {
+
+    fun providesApiService(retrofit:Retrofit):FoodRecipiesApi{
+        return
+    }
 }
