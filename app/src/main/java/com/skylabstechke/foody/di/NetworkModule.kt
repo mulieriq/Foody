0b89825b9254dfa6831776e980177c5656028e94
EricRootLee <erickiokom@gmail.com>
2021-04-12 13:52:12 +0300

    ProvideAPi service:

--- app/src/main/java/com/skylabstechke/foody/di/NetworkModule.kt
+++ app/src/main/java/com/skylabstechke/foody/di/NetworkModule.kt
@@ -2,15 +2,21 @@ package com.skylabstechke.foody.di
 
 import com.skylabstechke.foody.data.FoodRecipiesApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object NetworkModule {
 
+
+
+     @Singleton
+     @Provides
     fun providesApiService(retrofit:Retrofit):FoodRecipiesApi{
-        return
+        return retrofit.create(FoodRecipiesApi::class.java)
     }
 }
