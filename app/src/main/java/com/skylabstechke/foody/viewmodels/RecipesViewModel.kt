bc48a5ca53cc1ac32584c09aaf08fc14cf72612f
EricRootLee <erickiokom@gmail.com>
2021-04-13 02:19:14 +0300

    SMall refactor

--- app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
@@ -2,17 +2,24 @@ package com.skylabstechke.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.skylabstechke.foody.utilis.Constants
+import com.skylabstechke.foody.utilis.Constants.Companion.API_KEY
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_ADD_RECIPE_INFO
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_API_KEY
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_DIET
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_FILL_INGREDIENTS
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_NUMBER
+import com.skylabstechke.foody.utilis.Constants.Companion.QUERY_TYPE
+
 
 class RecipesViewModel(application: Application) : AndroidViewModel(application) {
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
-        queries[Constants.QUERY_NUMBER] = "50"
-        queries[Constants.QUERY_API_KEY] = Constants.API_KEY
-        queries[Constants.QUERY_TYPE] = "snack"
-        queries[Constants.QUERY_DIET] = "vegan"
-        queries[Constants.QUERY_ADD_RECIPE_INFO] = "true"
-        queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
+        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = "snack"
+        queries[QUERY_DIET] = "vegan"
+        queries[QUERY_ADD_RECIPE_INFO] = "true"
+        queries[QUERY_FILL_INGREDIENTS] = "true"
 
         return queries
     }
