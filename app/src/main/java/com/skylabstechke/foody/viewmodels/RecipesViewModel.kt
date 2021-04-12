f967c4088d2bd6bcfcfa0508e4ff352686db035a
EricRootLee <erickiokom@gmail.com>
2021-04-13 02:10:39 +0300

    moved applyQueries to recipe view model

--- app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
@@ -2,6 +2,21 @@ package com.skylabstechke.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.skylabstechke.foody.utilis.Constants
 
 class RecipesViewModel(application: Application):AndroidViewModel(application) {
+     fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries["number"] = "50"
+        queries["apiKey"] = Constants.API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+
+        return queries
+    }
+
+
+
 }
