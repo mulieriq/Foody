78316945a5ef296a00e275e85f435a7ab6c70d78
EricRootLee <erickiokom@gmail.com>
2021-04-12 18:15:42 +0300

    check  internet connection

--- app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.skylabstechke.foody.viewmodels
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
 import com.skylabstechke.foody.data.Repository
@@ -10,4 +13,20 @@ class MainViewModel @ViewModelInject constructor(
 
     application: Application
 ) : AndroidViewModel(application) {
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager =
+            getApplication<Application>().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return  when{
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)->true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)->true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)->true
+            else -> false
+
+
+        }
+    }
 }
