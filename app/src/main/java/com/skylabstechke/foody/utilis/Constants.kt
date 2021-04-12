34b0b98f36fe99b0805bff964fcccddc1c13f269
EricRootLee <erickiokom@gmail.com>
2021-04-13 02:17:21 +0300

    Queries constants

--- app/src/main/java/com/skylabstechke/foody/utilis/Constants.kt
+++ app/src/main/java/com/skylabstechke/foody/utilis/Constants.kt
@@ -4,5 +4,14 @@ class Constants {
     companion object {
         const val API_KEY = "3ebfff5f4fd849bea702b5b5df175767";
         const val BASE_URL = "https://api.spoonacular.com"
+
+        //API Query Keys
+        const val  QUERY_NUMBER = "number"
+        const val  QUERY_API_KEY= "apiKey"
+        const val  QUERY_TYPE= "type"
+        const val  QUERY_DIET = "diet"
+        const val  QUERY_ADD_RECIPE_INFO = "addRecipeInformation"
+        const val  QUERY_FILL_INGREDIENTS = "fillIngredients"
+
     }
 }
--- app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/RecipesViewModel.kt
@@ -4,19 +4,18 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.skylabstechke.foody.utilis.Constants
 
-class RecipesViewModel(application: Application):AndroidViewModel(application) {
-     fun applyQueries(): HashMap<String, String> {
+class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+    fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
-        queries["number"] = "50"
-        queries["apiKey"] = Constants.API_KEY
-        queries["type"] = "snack"
-        queries["diet"] = "vegan"
-        queries["addRecipeInformation"] = "true"
-        queries["fillIngredients"] = "true"
+        queries[Constants.QUERY_NUMBER] = "50"
+        queries[Constants.QUERY_API_KEY] = Constants.API_KEY
+        queries[Constants.QUERY_TYPE] = "snack"
+        queries[Constants.QUERY_DIET] = "vegan"
+        queries[Constants.QUERY_ADD_RECIPE_INFO] = "true"
+        queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
 
         return queries
     }
-
 
 
 }
