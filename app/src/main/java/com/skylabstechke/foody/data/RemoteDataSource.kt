ccef726f4f6d992859c966eccd71db675d8afcd1
EricRootLee <erickiokom@gmail.com>
2021-04-12 15:25:00 +0300

    Get recipes

--- app/src/main/java/com/skylabstechke/foody/data/RemoteDataSource.kt
+++ app/src/main/java/com/skylabstechke/foody/data/RemoteDataSource.kt
@@ -1,7 +1,12 @@
 package com.skylabstechke.foody.data
+import com.skylabstechke.foody.models.FoodRecipe
+import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteDataSource  @Inject constructor(
     private val foodRecipiesApi: FoodRecipiesApi
 ) {
+  suspend  fun getRecipes(queries:Map<String,String>) : Response<FoodRecipe>{
+      return  foodRecipiesApi.getRecipes(queries)
+    }
 }
