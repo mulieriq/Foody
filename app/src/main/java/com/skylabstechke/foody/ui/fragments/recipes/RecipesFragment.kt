b3e2c742688bf0d19c7c5e288dca10e39641afad
EricRootLee <erickiokom@gmail.com>
2021-04-13 01:14:49 +0300

    Init viewmodel

--- app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.skylabstechke.foody.R
 import com.skylabstechke.foody.adapters.RecipesRecyclerViewAdapter
+import com.skylabstechke.foody.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
     private val mAdapter by lazy { RecipesRecyclerViewAdapter() }
+    private lateinit var mainViewModel:MainViewModel
 
     private lateinit var mView: View
 
@@ -22,6 +25,7 @@ class RecipesFragment : Fragment() {
     ): View? {
 
         mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         setupRecyclerView()
         return mView
     }
@@ -41,4 +45,6 @@ class RecipesFragment : Fragment() {
         mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
         showShimmerEffect()
     }
+
+    private fun requestApiData(){}
 }
