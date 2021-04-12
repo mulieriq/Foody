d9cc56e69d7eefb1866066b897f3d9d2b25499d0
EricRootLee <erickiokom@gmail.com>
2021-04-12 18:27:05 +0300

    api call

--- app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
@@ -6,7 +6,12 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.skylabstechke.foody.data.Repository
+import com.skylabstechke.foody.models.FoodRecipe
+import com.skylabstechke.foody.utilis.NetworkResult
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     private val repository: Repository,
@@ -14,19 +19,30 @@ class MainViewModel @ViewModelInject constructor(
     application: Application
 ) : AndroidViewModel(application) {
 
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+    fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
+        getRecipesSafeCall(queries)
+    }
+
+    private fun getRecipesSafeCall(queries: Map<String, String>) {
+        if (hasInternetConnection()) {
+
+        }else{
+            recipesResponse.value = NetworkResult.Error("No Internet Connection")
+        }
+    }
+
     private fun hasInternetConnection(): Boolean {
         val connectivityManager =
             getApplication<Application>().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
 
-        return  when{
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)->true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)->true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)->true
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
             else -> false
-
-
         }
     }
 }
