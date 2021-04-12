d17965e3fe0022839886306c5cd68757f2ef3dbb
EricRootLee <erickiokom@gmail.com>
2021-04-13 01:12:01 +0300

    Set up recyler view

--- app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.skylabstechke.foody.R
+import com.skylabstechke.foody.adapters.RecipesRecyclerViewAdapter
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
+    private val mAdapter by lazy { RecipesRecyclerViewAdapter() }
 
     private lateinit var mView: View
 
@@ -19,7 +22,7 @@ class RecipesFragment : Fragment() {
     ): View? {
 
         mView = inflater.inflate(R.layout.fragment_recipes, container, false)
-
+        setupRecyclerView()
         return mView
     }
 
@@ -31,5 +34,11 @@ class RecipesFragment : Fragment() {
     private fun hideShimmerEffect() {
 
         mView.recyclerview.hideShimmer()
+    }
+
+    private fun setupRecyclerView() {
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
     }
 }
