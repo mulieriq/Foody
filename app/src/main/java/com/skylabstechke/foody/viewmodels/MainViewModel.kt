5638f7629bd450368c5a2852df9aa1caa662dd6f
EricRootLee <erickiokom@gmail.com>
2021-04-12 18:05:57 +0300

    Viewmdel inject with repository

--- app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.skylabstechke.foody.viewmodels
 
 import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import com.skylabstechke.foody.data.Repository
 
-class MainViewModel(application: Application) :AndroidViewModel(application) {
+class MainViewModel @ViewModelInject constructor(
+    private val repository: Repository,
+
+    application: Application
+) : AndroidViewModel(application) {
 }
