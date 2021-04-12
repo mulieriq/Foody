d7c5bd825ee5f8b3c805d96173464a24b290370e
EricRootLee <erickiokom@gmail.com>
2021-04-13 00:00:52 +0300

    Bind adapters

--- app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesRowBindingAdapter.kt
+++ app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesRowBindingAdapter.kt
@@ -1,7 +1,11 @@
 package com.skylabstechke.foody.bindingadapters
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.skylabstechke.foody.R
 
 class RecipesRowBindingAdapter {
 
@@ -12,5 +16,29 @@ class RecipesRowBindingAdapter {
         fun setNumberOfLikes(textView: TextView, likes: Int) {
             textView.text = likes.toString()
         }
+
+        @BindingAdapter("setNumberOfMinutes")
+        @JvmStatic
+        fun setNumberOfMinutes(textView: TextView, likes: Int) {
+            textView.text = likes.toString()
+        }
+
+        @BindingAdapter("applyVeganColor")
+        @JvmStatic
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
+                        view.setTextColor(ContextCompat.getColor(view.context, R.color.green))
+                    }
+                    is ImageView -> {
+                        view.setColorFilter(ContextCompat.getColor(view.context, R.color.green))
+                    }
+                }
+            }
+
+
+        }
+
     }
 }
