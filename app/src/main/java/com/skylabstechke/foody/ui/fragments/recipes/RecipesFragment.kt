67ab824ee586eb9af3ae32b7a5ac61dec9d0cec4
EricRootLee <erickiokom@gmail.com>
2021-04-13 00:59:22 +0300

    Hiding and showing shimmer effect

--- app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -1,22 +1,35 @@
 package com.skylabstechke.foody.ui.fragments.recipes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.skylabstechke.foody.R
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private lateinit var mView: View
 
-        val view =  inflater.inflate(R.layout.fragment_recipes, container, false)
-        view.recyclerview.showShimmer()
-        return  view
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        return mView
     }
 
+    private fun showShimmerEffect() {
+
+        mView.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+
+        mView.recyclerview.hideShimmer()
+    }
 }
