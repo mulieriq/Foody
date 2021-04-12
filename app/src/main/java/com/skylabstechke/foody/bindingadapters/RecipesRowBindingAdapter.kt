e513f823a11d5c2efea110180d6df47ebd231161
EricRootLee <erickiokom@gmail.com>
2021-04-12 23:46:53 +0300

    Bind adapter

--- app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesRowBindingAdapter.kt
+++ app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesRowBindingAdapter.kt
@@ -1,4 +1,16 @@
 package com.skylabstechke.foody.bindingadapters
 
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
 class RecipesRowBindingAdapter {
+
+
+    companion object {
+        @BindingAdapter("setNumberOfLikes")
+        @JvmStatic
+        fun setNumberOfLikes(textView: TextView, likes: Int) {
+            textView.text = likes.toString()
+        }
+    }
 }
