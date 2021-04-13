ae94b7cbb8c9e75d0bc4956ad3ad2b97059debf7
EricRootLee <erickiokom@gmail.com>
2021-04-13 20:18:15 +0300

    TO strimg

--- app/src/main/java/com/skylabstechke/foody/data/room/RecipesTypeConverter.kt
+++ app/src/main/java/com/skylabstechke/foody/data/room/RecipesTypeConverter.kt
@@ -1,4 +1,16 @@
 package com.skylabstechke.foody.data.room
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.skylabstechke.foody.models.FoodRecipe
+
 class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipesToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
 }
