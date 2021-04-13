9e05f9a403fba188b40c411e2125dc481d4f3bd7
EricRootLee <erickiokom@gmail.com>
2021-04-13 20:58:09 +0300

    Database module

--- app/src/main/java/com/skylabstechke/foody/data/room/RecipesDatabase.kt
+++ app/src/main/java/com/skylabstechke/foody/data/room/RecipesDatabase.kt
@@ -2,7 +2,14 @@ package com.skylabstechke.foody.data.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-abstract class RecipesDatabase  : RoomDatabase(){
-
+@Database(
+    entities = [RecipesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase : RoomDatabase() {
+    abstract fun recipesDao(): RecipesDao
 }
