e7f8e362f770151e70539320d9cb4757f713d83c
EricRootLee <erickiokom@gmail.com>
2021-04-13 21:38:06 +0300

    done

--- app/src/main/java/com/skylabstechke/foody/di/DatabaseModule.kt
+++ app/src/main/java/com/skylabstechke/foody/di/DatabaseModule.kt
@@ -5,18 +5,25 @@ import androidx.room.Room
 import com.skylabstechke.foody.data.room.RecipesDatabase
 import com.skylabstechke.foody.utilis.Constants.Companion.DATABASE_NAME
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object DatabaseModule {
 
-
+    @Singleton
+    @Provides
     fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
         context,
         RecipesDatabase::class.java,
         DATABASE_NAME
-    )
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRecipeDao (database: RecipesDatabase) = database.recipesDao()
 }
