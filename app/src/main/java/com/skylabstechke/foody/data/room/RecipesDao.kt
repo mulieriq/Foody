7be0b637d40d85bd0032fd629de63ace8b8fac34
EricRootLee <erickiokom@gmail.com>
2021-04-13 20:10:15 +0300

    Using flow

--- app/src/main/java/com/skylabstechke/foody/data/room/RecipesDao.kt
+++ app/src/main/java/com/skylabstechke/foody/data/room/RecipesDao.kt
@@ -1,8 +1,18 @@
 package com.skylabstechke.foody.data.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface RecipesDao {
+    @Insert
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+
+
 }
