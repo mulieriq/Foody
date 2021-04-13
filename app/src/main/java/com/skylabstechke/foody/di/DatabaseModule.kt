78cf122d918a4c6b61fb0a13bb7df24cd82739de
EricRootLee <erickiokom@gmail.com>
2021-04-13 21:11:33 +0300

    Providing database instance

--- app/src/main/java/com/skylabstechke/foody/di/DatabaseModule.kt
+++ app/src/main/java/com/skylabstechke/foody/di/DatabaseModule.kt
@@ -1,4 +1,22 @@
 package com.skylabstechke.foody.di
 
+import android.content.Context
+import androidx.room.Room
+import com.skylabstechke.foody.data.room.RecipesDatabase
+import com.skylabstechke.foody.utilis.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ApplicationComponent::class)
 object DatabaseModule {
+
+
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    )
 }
