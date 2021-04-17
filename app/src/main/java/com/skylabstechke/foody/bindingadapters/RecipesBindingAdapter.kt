163dc58c4d70260f1f25a9f69611a2943eddd5da
EricRootLee <erickiokom@gmail.com>
2021-04-18 00:24:28 +0300

    bind adapter

--- app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesBindingAdapter.kt
+++ app/src/main/java/com/skylabstechke/foody/bindingadapters/RecipesBindingAdapter.kt
@@ -2,6 +2,7 @@ package com.skylabstechke.foody.bindingadapters
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.skylabstechke.foody.data.room.RecipesEntity
 import com.skylabstechke.foody.models.FoodRecipe
@@ -14,17 +15,38 @@ class RecipesBindingAdapter {
         @JvmStatic
         fun errImageViewVisibility(
             imageView: ImageView,
-            apiResponse: NetworkResult<FoodRecipe?>,
-            database: List<RecipesEntity>
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
         ) {
 
             if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
                 imageView.visibility = View.VISIBLE
-            }else if (apiResponse is NetworkResult.Loading){
+            } else if (apiResponse is NetworkResult.Loading) {
                 imageView.visibility = View.INVISIBLE
-            }else if (apiResponse is NetworkResult.Success){
+            } else if (apiResponse is NetworkResult.Success) {
                 imageView.visibility = View.INVISIBLE
             }
+        }
+
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+        @JvmStatic
+        fun errTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+
+        ) {
+
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+
+
         }
     }
 }
