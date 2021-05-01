21f9269193ba4df4833512b15709c956b4e5cf1e
EricRootLee <erickiokom@gmail.com>
2021-05-01 12:36:56 +0300

    meal type data class

--- app/src/main/java/com/skylabstechke/foody/data/DataStoreRepository.kt
+++ app/src/main/java/com/skylabstechke/foody/data/DataStoreRepository.kt
@@ -1,7 +1,10 @@
 package com.skylabstechke.foody.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -17,4 +20,16 @@ class DataStoreRepository @Inject constructor(@ApplicationContext private  val c
 
     }
 
+    private val datastore :DataStore<Preferences> = context.createDataStore(
+        name = "foody_preferences"
+    )
+
 }
+
+data class MealAndDietType(
+val selectedMealType:String,
+val selectedMealTypeId : Int,
+val selectedDietType:String,
+val selectedDietTypeId: Int
+
+)
