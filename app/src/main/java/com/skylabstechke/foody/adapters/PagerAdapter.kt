57a0c260ac6da5324637eeca32f42299b9e92896
EricRootLee <erickiokom@gmail.com>
2021-05-10 17:58:52 +0300

    done with adapter

--- app/src/main/java/com/skylabstechke/foody/adapters/PagerAdapter.kt
+++ app/src/main/java/com/skylabstechke/foody/adapters/PagerAdapter.kt
@@ -1,4 +1,26 @@
 package com.skylabstechke.foody.adapters
 
-class PagerAdapter {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(
+    private val results: Bundle,
+    private val fragments: ArrayList<Fragment>,
+    private val title: ArrayList<String>,
+    fm: FragmentManager
+) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return title[position]
+    }
+
+    override fun getItem(position: Int): Fragment {
+        fragments[position].arguments = results
+        return fragments[position]
+    }
 }
