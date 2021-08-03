f1922e7490f19a478598d537c0784ca1e39058b3
EricRootLee <erickiokom@gmail.com>
2021-08-03 15:47:38 +0300

    WOrking on the view

--- app/src/main/java/com/skylabstechke/foody/ui/fragments/favorities/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/skylabstechke/foody/ui/fragments/favorities/FavoriteRecipesFragment.kt
@@ -11,7 +11,13 @@ class FavoriteRecipesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+       val view =  inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        setUpRecyclerView(view)
+        return view
+    }
+
+    private fun setUpRecyclerView(view: View){
+
     }
 
 }
