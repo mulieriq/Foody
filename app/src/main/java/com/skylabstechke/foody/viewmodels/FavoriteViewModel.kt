df38f6f066cda92b43b318fb031f638d2d43ff02
EricRootLee <erickiokom@gmail.com>
2021-08-03 14:51:39 +0300

    Favorite view model

---
+++ app/src/main/java/com/skylabstechke/foody/viewmodels/FavoriteViewModel.kt
@@ -0,0 +1,8 @@
+package com.skylabstechke.foody.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+
+class FavoriteViewModel @ViewModelInject constructor() : ViewModel() {
+}
